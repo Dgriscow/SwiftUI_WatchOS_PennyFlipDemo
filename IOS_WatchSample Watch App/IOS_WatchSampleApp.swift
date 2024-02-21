d91b81eba46461167f9548f5a64a5036a7b46fe2
devingrischow <devingrischow@outlook.com>
2024-02-21 10:04:31 -0600

    Initial Commit

---
+++ IOS_WatchSample Watch App/IOS_WatchSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_WatchSampleApp.swift
+//  IOS_WatchSample Watch App
+//
+//  Created by Devin Grischow on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct IOS_WatchSample_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
